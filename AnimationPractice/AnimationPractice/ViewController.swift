24061d6ace938067fe553e641a68e62d20fe2ae1
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-06 16:09:17 -0600

    Started Animation app

---
+++ AnimationPractice/AnimationPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AnimationPractice
+//
+//  Created by Taylor Frost on 7/6/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+}
+
