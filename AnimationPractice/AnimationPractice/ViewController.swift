fa8de9e8cf0376afae0c3cbbe7cb21e7ea3f9319
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-06 19:19:17 -0600

    Got label UIImageView Moving

--- AnimationPractice/AnimationPractice/ViewController.swift
+++ AnimationPractice/AnimationPractice/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var swiftOutlet: UIImageView!
+    
+    var timer : NSTimer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         
+ self.timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(moveButterfly), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    func moveButterfly() {
+        let width = self.view.frame.size.width
+        let height = self.view.frame.size.height
+        
+        UIView.animateWithDuration(2.0) {
+            
+        }
+        
+        UIView.animateWithDuration(2.0) {
+            self.swiftOutlet.center = CGPoint(x: 0, y: 0)
+            
+        }
     }
 
 
